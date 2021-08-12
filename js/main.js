b972842b98b43021a42d16bf965730adec6f27ea
Seydaspferd <damethe@hotmail.com>
2021-08-12 16:00:12 +0200

    updated english about us pictures and descriptions, history and started updating the gallery site

--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ const scroll = new SmoothScroll('.datebar a[href*="#"]', {
 const scroll2 = new SmoothScroll('.iconbar a[href*="#"]', {
   speed: 500
 });
+
+
+const scroll3 = new SmoothScroll('.projectlinks a[href*="#"]', {
+  speed: 500
+});
